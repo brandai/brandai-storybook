55c8b0ae16fdf67f2218682afac656c9bb69a024
kimmajor <kim@brand.ai>
2017-06-12 07:52:33 +0300

    upgrade to storybook 3

--- src/manager.js
+++ src/manager.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import addons from '@kadira/storybook-addons';
+import addons from '@storybook/addons';
 import AssetContainer from './addon/AssetContainer';
 import {ADDON_ID, PANEL_ID} from './constants';
 
